2cdd43addff5b5c1cbf3a12c9708a9d1454ddbc2
Rektagun <khatrisurya.101202@gmail.com>
2024-09-30 16:02:14 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './output.css'
+
+function App() {
+  return (
+
+    <header>
+      <h1 className="font-bold bg-blue-200 border-black border justify-center flex">
+        Suryaprakash Khatri
+      </h1>
+    </header>
+
+  );
+}
+
+export default App;
